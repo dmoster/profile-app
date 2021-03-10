62deba681d8b4dab75f6ff55674440a4703ae171
Daniel Moster <22249073+dmoster@users.noreply.github.com>
2021-03-10 01:17:55 -0700

    add style and initial content for profile page

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:profile_app/widgets/profile_list_tile.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key key}) : super(key: key);
@@ -10,10 +11,68 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  String imageUrl = 'assets/images/me_square.png';
+  String firstName = 'Daniel';
+  String lastName = 'Moster';
+  String phoneNumber = '(513) 725-7100';
+  String emailAddress = 'd.moster@me.com';
+  String bioText =
+      'Hi, my name is Daniel Moster. I am from Ohio but live in Nampa, ID.';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 96),
+              Text(
+                'Edit Profile',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16),
+              CircleAvatar(
+                backgroundColor: Colors.blue,
+                radius: 72,
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/me_square.png'),
+                  radius: 66,
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.symmetric(vertical: 0),
+                  children: [
+                    ProfileListTile(
+                      label: 'Name',
+                      content: '$firstName $lastName',
+                    ),
+                    ProfileListTile(
+                      label: 'Phone',
+                      content: '$phoneNumber',
+                    ),
+                    ProfileListTile(
+                      label: 'Email',
+                      content: '$emailAddress',
+                    ),
+                    ProfileListTile(
+                      label: 'Tell us about yourself',
+                      content: '$bioText',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/profile_list_tile.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ProfileListTile extends StatefulWidget {
+  const ProfileListTile({Key key, @required this.label, @required this.content})
+      : super(key: key);
+
+  final String label;
+  final String content;
+
+  @override
+  _ProfileListTileState createState() => _ProfileListTileState();
+}
+
+class _ProfileListTileState extends State<ProfileListTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colors.black12),
+        ),
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.all(0),
+        isThreeLine: widget.content.length > 45 ? true : false,
+        title: Text(
+          widget.label,
+          style: TextStyle(
+            color: Colors.black26,
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(widget.content,
+            style: TextStyle(
+              color: Color(0xff323232),
+              fontWeight: FontWeight.bold,
+            )),
+        trailing: Transform.scale(
+          scale: 1.5,
+          child: Icon(
+            Icons.chevron_right_outlined,
+            color: Colors.black26,
+          ),
+        ),
+      ),
+    );
+  }
+}
